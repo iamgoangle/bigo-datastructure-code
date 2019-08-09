c26f4c3bded8f32fe1f3db4c633c53d8b9451a8b
Teerapong Singthong <st.teerapong@gmail.com>
2019-08-09 22:40:09 +0700

    feat: refactor to non recursive

--- bigo/OlogN.go
+++ bigo/OlogN.go
@@ -24,33 +24,33 @@ import (
 
 func main() {
 	lists := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	find := 3
+	find := 7
 
-	fmt.Println(binarySearch(lists, 0, len(lists)-1, find))
+	fmt.Println(binarySearch(lists, find))
 }
 
-func binarySearch(lists []int, left, right, find int) (string, error) {
-	if right < left {
-		return "", errors.New("left must not greather than right")
+func binarySearch(lists []int, find int) (string, error) {
+	low := 0
+	high := len(lists) - 1
+
+	for {
+		if low <= high {
+			var mid int = low + (high-low)/2
+
+			if find < lists[mid] {
+				high = mid - 1
+			} else if find > lists[mid] {
+				low = mid + 1
+			} else {
+				return "found", nil
+			}
+
+			fmt.Println("low => ", low)
+			fmt.Println("mid => ", mid)
+			fmt.Println("high => ", high)
+			fmt.Println("====")
+		} else {
+			return "", errors.New("not found")
+		}
 	}
-
-	mid := left + ((right - left) / 2)
-	fmt.Println("left = ", left)
-	fmt.Println("mid = ", mid)
-	fmt.Println("right = ", right)
-	fmt.Println("=====")
-
-	if lists[mid] == find {
-		return fmt.Sprintf("found %v", lists[mid]), nil
-	}
-
-	if lists[mid] > find {
-		return binarySearch(lists, left, mid-1, find)
-	}
-
-	if lists[mid] < find {
-		return binarySearch(lists, mid+1, right, find)
-	}
-
-	return "", nil
 }
